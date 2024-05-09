d5d9ae545abce9d4af0bb0b074c9855c9b35005b
Rubinich <alojzrubinic@gmail.com>
2024-05-09 19:26:02 +0200

    animations on add

--- src/layouts/add/add.js
+++ src/layouts/add/add.js
@@ -1,17 +1,23 @@
 import "./add.css";
 import addBtn from "./../../assets/add-icon.svg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function AddNote() {
     const navigate = useNavigate()
     const goToMakeNote = () => {
         navigate("/make-note")
     }
+    const [isClicked, setIsClicked] = useState(false);
+    const addBtnClass = isClicked ? "add-btn clicked" : "add-btn";
+    const isAddClicked = () => {
+        setIsClicked(!isClicked);
+    };
 
     return (
         <div className="add-wrapper">
-            <div className="add-btn" onClick={() => goToMakeNote()}>
-                <button><img src={addBtn} alt=""/></button>
+            <div className={addBtnClass} onClick={isAddClicked} onTransitionEnd={goToMakeNote}>
+                <button><img src={addBtn} alt="" /></button>
             </div>
         </div>
     );
