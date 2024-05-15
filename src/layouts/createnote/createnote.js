6b540b944ba25a4ae344de25f2ff09b6d5d9aa39
Rubinich <alojzrubinic@gmail.com>
2024-05-15 18:59:52 +0200

    notes

--- src/layouts/createnote/createnote.js
+++ src/layouts/createnote/createnote.js
@@ -1,20 +1,34 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import HeaderAdd from "../header-add/header-add.js";
 
 
-function CreateNote({ textHandler, saveHandler, inputText }) {
+function CreateNote({ textHandler, saveHandler, inputText , maxLength }) {
+    const [textareaHeight, setTextareaHeight] = useState("auto");
+    const [remainingCharacters, setRemainingCharacters] = useState(maxLength);
+
+    useEffect(() => {
+        setTextareaHeight(`${inputText.split("\n").length * 1.2}em`);
+        setRemainingCharacters(maxLength - inputText.length);
+    }, [inputText]);
+    maxLength = 1000;
+
+
+
     return (
         <>
-            <HeaderAdd saveHandler={saveHandler}/>
-            <div className="note" style={{ background: "rgb(43, 108, 255)" }}> 
+            <HeaderAdd saveHandler={saveHandler} />
+            <div className="note" style={{ background: "rgb(43, 108, 255)" }}>
                 <textarea
-                    cols="10"
-                    rows="10"
+
                     value={inputText}
                     placeholder="Enter note"
                     onChange={textHandler}
-                    maxLength="500"
+                    maxLength={maxLength}
+                    style={{ height: textareaHeight, resize: "none" }}
                 ></textarea>
+                <div className="character-counter">
+                    {remainingCharacters} characters remaining
+                </div>
             </div>
         </>
     );
